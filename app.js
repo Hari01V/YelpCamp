69e6e78cffc256bd4f44942bf1d18ae8a18bc5f6
Hari01V <hari01venkat@gmail.com>
2020-11-25 08:47:12 +0000

    error resolving for app.listen port

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.use(authRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, function(){
 	console.log("YelpCamp Server StArTeD ");
 });
